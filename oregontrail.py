b61267fe0e3538ee54deb5972b1f039826becd14
RG-Mac <ramong5406@gmail.com>
2020-12-07 17:51:27 -0600

    created first question in game

--- oregontrail.py
+++ oregontrail.py
@@ -1,2 +1,14 @@
-print('oregon trail')
-print('hello')
+import random
+import time
+
+#welcome player to game
+print("Welcome to the Oregon Trail")
+
+#Ask for Name
+playerName = input("What's your name?")
+while len(playerName) >= 0:
+    if len(playerName) > 0:
+        print(str(playerName) + " is a great name!")
+        break
+
+
