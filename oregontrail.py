8c926fa55aa3197dd745991e7b09ec57aaf06d8a
RG-Mac <ramong5406@gmail.com>
2020-12-08 01:02:52 -0600

    created second question for player's name

--- oregontrail.py
+++ oregontrail.py
@@ -7,8 +7,14 @@ print("Welcome to the Oregon Trail")
 #Ask for Name
 playerName = input("What's your name?")
 while len(playerName) >= 0:
-    if len(playerName) > 0:
+    if len(playerName) > 1:
         print(str(playerName) + " is a great name!")
         break
+    if len(playerName) == 1:
+        playerNameChoice = input(str(playerName) + "? Is your name really only 1 letter?(y/n):")
+        if playerNameChoice == "y" or playerNameChoice == "Y":
+            print("Well, that's great, too!...")
+            break
+
 
 
