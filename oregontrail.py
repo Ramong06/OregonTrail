56c231c1af214ddc192741d49a40360a439ffe56
RG-Mac <ramong5406@gmail.com>
2020-12-08 11:42:38 -0600

    finished ask name section of game

--- oregontrail.py
+++ oregontrail.py
@@ -2,7 +2,7 @@ import random
 import time
 
 #welcome player to game
-print("Welcome to the Oregon Trail")
+print("Welcome to the South Texas Trail!")
 
 #Ask for Name
 playerName = input("What's your name?")
@@ -11,10 +11,16 @@ while len(playerName) >= 0:
         print(str(playerName) + " is a great name!")
         break
     if len(playerName) == 1:
-        playerNameChoice = input(str(playerName) + "? Is your name really only 1 letter?(y/n):")
+        playerNameChoice = input(str(playerName) + "? Is your name really only 1 letter? (y/n):")
         if playerNameChoice == "y" or playerNameChoice == "Y":
-            print("Well, that's great, too!...")
+            print("Well, that's great!")
             break
+        if playerNameChoice == "n" or playerNameChoice == "N":
+            playerName = input("Well, what is your name?")
+        else:
+            print("You didn't type anything, try again :) ")
+            playerName = input("What is your name?")
+
 
 
 
